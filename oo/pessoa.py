2b869825c91b3f4d3acfcb20e2f120f6e76d50ce
GustavoKoiti <gugagko@gmail.com>
2020-06-09 21:39:04 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,18 +1,21 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=37):
+    def __init__(self, *filhos, nome=None, idade=37):
         self.idade = idade
         self.nome = nome
-
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá Sr. {id(self)}'
 
+
 if __name__ == '__main__':
-    p = Pessoa('Gustavo')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Gustavo Koiti'
-    print(p.nome)
-    print(p.idade)
+    gustavo = Pessoa(nome='Gustavo')
+    luciano = Pessoa(gustavo, nome='Luciano')
+    print(Pessoa.cumprimentar(luciano))
+    print(id(luciano))
+    print(luciano.cumprimentar())
+    print(luciano.nome)
+    print(luciano.idade)
+    for filho in luciano.filhos:
+        print(filho.nome)
+
