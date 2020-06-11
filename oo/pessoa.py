d7ad8648b85a0df2266d3ded4bbc9ac6adc2e99d
GustavoKoiti <gugagko@gmail.com>
2020-06-10 21:39:10 -0300

    Criado atributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,8 +1,11 @@
+# Pacote -> Módulo -> Classe -> Método("função")
 class Pessoa:
+    olhos = 2  # atributo de classe, se for geral para todas as "Pessoa"
+
     def __init__(self, *filhos, nome=None, idade=37):
-        self.idade = idade
-        self.nome = nome
-        self.filhos = list(filhos)
+        self.idade = idade  # atributos de instância
+        self.nome = nome  # atributos de instância
+        self.filhos = list(filhos)  # atributos de instância
 
     def cumprimentar(self):
         return f'Olá Sr. {id(self)}'
@@ -22,4 +25,6 @@ if __name__ == '__main__':
     del luciano.filhos
     print(luciano.__dict__)
     print(gustavo.__dict__)
-
+    print(Pessoa.olhos)
+    print(gustavo.olhos)
+    print(luciano.olhos)
